97fa90ed678a85a49733e492b8120882a472eb7e
meilani <lauraho2006@gmail.com>
2020-04-09 13:34:48 -0400

    email checking

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 let photo = document.querySelector('#photo');
+let submit = document.querySelector('#submit');
+let email = document.querySelector('.email-address');
+let error = document.querySelector('.error');
+let invalid = document.querySelector('.invalid-email');
+let valid = document.querySelector('.valid-email');
+
 
 function getPhoto(e) {
     if (e.target.innerWidth <= 525) {
@@ -8,4 +14,29 @@ function getPhoto(e) {
     }
 }
 
+function checkEmail(e) {
+    e.preventDefault();
+    console.log('checkEmail');
+    var email = document.querySelector('.email-address');
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if (!filter.test(email.value)) {
+    error.style.display = "block";
+    invalid.style.display = "block";
+    valid.style.display = "none";
+    email.focus;
+    return false;
+ } else {
+    error.style.display = "none";
+    invalid.style.display = "none";
+    valid.style.display = "block";
+    email.focus;
+    email.value = "";
+    return false;
+ }
+
+}
+
+
 window.addEventListener('resize', getPhoto);
+submit.addEventListener('click', checkEmail);
